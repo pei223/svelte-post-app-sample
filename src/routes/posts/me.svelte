9177ca8e7af685ecb8f79db579a9931d8edd4439
pei223 <ippei.223miyake119@gmail.com>
2022-02-03 09:20:42 +0900

    記事削除画面

--- src/routes/posts/me.svelte
+++ src/routes/posts/me.svelte
@@ -18,11 +18,13 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { getStores } from '$app/stores';
-	import { getMyPosts } from '$lib/apis/postApi';
+	import { deletePost, getMyPosts } from '$lib/apis/postApi';
 	import type { MyPost } from '$lib/domain/post';
 	import { AppStoreType, AppStoreWrapper } from '$lib/stores/AppStore';
 	import { get } from 'svelte/store';
 	import { onMount } from 'svelte';
+	import Dialog, { Title, Content, Actions } from '@smui/dialog';
+	import Button, { Label } from '@smui/button';
 	import Heading from '$lib/components/atoms/Heading.svelte';
 	import PagingNav from '$lib/components/blocks/PagingNav.svelte';
 	import { ERROR_CODE, genErrorPath } from '$lib/domain/error';
@@ -38,6 +40,8 @@
 	export let page = 1;
 	let maxPage = 1;
 	let loading = true;
+	let deleteConfirmDialogOpen = false;
+	let deleteTargetIndex = -1;
 
 	const appStore = get<AppStoreType>(getStores().session);
 
@@ -46,6 +50,7 @@
 			fetchPost(page);
 		}
 	}
+	$: deleteTargetPost = posts ? posts[deleteTargetIndex] : null;
 	onMount(async () => {
 		fetchPost(page);
 	});
@@ -87,6 +92,45 @@
 	const onPageChanged = (page: number) => {
 		goto(`/posts/me?page=${page + 1}`);
 	};
+
+	const confirmDelete = (i: number, post: MyPost) => {
+		deleteTargetIndex = i;
+		deleteConfirmDialogOpen = true;
+	};
+
+	const doDeletePost = async () => {
+		if (deleteTargetIndex < 0) {
+			return;
+		}
+		try {
+			loading = true;
+			await deletePost(appStore.accessToken, deleteTargetPost.id);
+			loading = false;
+			posts.splice(deleteTargetIndex, 1);
+			posts = posts;
+			deleteTargetIndex = -1;
+			deleteConfirmDialogOpen = false;
+		} catch (e) {
+			if (!axios.isAxiosError(e)) {
+				goto(genErrorPath(this.$route.path, ERROR_CODE.notAxiosError));
+				return;
+			}
+			if (!e.response) {
+				goto(genErrorPath(this.$route.path, ERROR_CODE.networkError));
+				return;
+			}
+			switch (e.response.status) {
+				case 401:
+					goto(`/auth/login?redirectUrl=${location.hostname}`);
+					return;
+				case 404:
+					goto('/404');
+					return;
+				default:
+					goto(genErrorPath(this.$route.path, ERROR_CODE.unexpectedApiError));
+			}
+		}
+	};
 </script>
 
 <svelte:head>
@@ -98,7 +142,7 @@
 	{#if !loading}
 		{#each posts as post, i}
 			<div class="card-container">
-				<MyPostCard {post} {i} onDeleteClicked={(_, __) => {}} />
+				<MyPostCard {post} {i} onDeleteClicked={confirmDelete} />
 			</div>
 		{:else}
 			<div>データはありません</div>
@@ -109,6 +153,30 @@
 		<AddFab on:click={() => goto('/posts/add')} />
 	{/if}
 </section>
+
+{#if deleteTargetPost}
+	<Dialog
+		bind:open={deleteConfirmDialogOpen}
+		aria-labelledby="dialog-title"
+		aria-describedby="dialog-content"
+	>
+		<Title id="dialog-title">削除確認</Title>
+		<Content id="dialog-content">[{deleteTargetPost.title}]を削除します。よろしいですか？</Content>
+		<Actions>
+			<Button on:click={doDeletePost}>
+				<Label>OK</Label>
+			</Button>
+			<Button
+				on:click={() => {
+					deleteConfirmDialogOpen = false;
+					deleteTargetIndex = -1;
+				}}
+			>
+				<Label>Cancel</Label>
+			</Button>
+		</Actions>
+	</Dialog>
+{/if}
 
 <Loading open={loading} />
 
